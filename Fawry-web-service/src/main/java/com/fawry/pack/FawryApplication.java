47341183de3923d88e3d3344c785cc4ce7ff9506
mahmoud sayed <mahmoudsayed1332002@gmail.com>
2022-12-31 13:58:25 +0200

    initiate the project

---
+++ Fawry-web-service/src/main/java/com/fawry/pack/FawryApplication.java
@@ -0,0 +1,12 @@
+package com.fawry.pack;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FawryApplication.class, args);
+	}
+
+}
